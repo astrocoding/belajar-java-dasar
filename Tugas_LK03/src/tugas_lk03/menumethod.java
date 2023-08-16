db5658dbd12a955e289e774f8d82b2b303139662
Zaenal Alfian <118641939+Kodetopia@users.noreply.github.com>
2023-08-16 08:39:14 +0700

    add tugas_lk03
    Belajar GUI dan CRUD dasar dengan Java

---
+++ Tugas_LK03/src/tugas_lk03/menumethod.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_lk03;
+
+/**
+ *
+ * @author zaenalalfian
+ */
+public class menumethod {
+    public static void main(String[] args) {
+      
+        
+    }
+    public void quit(){
+        Menu mn = new Menu();        
+        mn.setVisible(true);
+        mn.setEnabled(true);
+        mn.setLocation(10, 10);
+        }
+    
+  
+    
+}
