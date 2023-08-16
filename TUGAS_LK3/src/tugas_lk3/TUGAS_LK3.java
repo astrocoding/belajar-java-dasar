715766f7f49c78285110981ea82368808450a10c
Zaenal Alfian <118641939+Kodetopia@users.noreply.github.com>
2023-08-16 08:41:11 +0700

    add tugas_lk3 remake

---
+++ TUGAS_LK3/src/tugas_lk3/TUGAS_LK3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_lk3;
+
+/**
+ *
+ * @author zaenalalfian
+ */
+public class TUGAS_LK3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Form1 satu = new Form1();
+        satu.setVisible(true);
+    }
+    
+}
